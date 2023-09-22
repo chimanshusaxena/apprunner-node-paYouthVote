33cbd266e813529d9878c7a283ef530d38a78353
Himanshu Saxena <143574238+chimanshusaxena@users.noreply.github.com>
2023-09-23 00:04:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-//app.get('/', (req, res) => {
-//   res.send('Hello Citi Team!');
-// });
-app.get('/', function(req, res){
-    res.render('/index.html');
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
